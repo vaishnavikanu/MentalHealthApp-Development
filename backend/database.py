2d0899ec72c13c20d1ecb63a07e1ace939e2a851
Kanukuntla Vaishnavi <148687223+vaishnavikanu@users.noreply.github.com>
2026-06-23 14:38:42 +0530

    removed password

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:5916vaish@localhost/mental_health_db"
+DATABASE_URL = "postgresql://postgres:Password@localhost/mental_health_db"
 
 engine = create_engine(DATABASE_URL)
 
